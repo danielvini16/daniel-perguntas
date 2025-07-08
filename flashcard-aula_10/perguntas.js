4e711565c4529e51c2e1a13b840fe544bf2f7b88
danielvini16 <danikjjk13@gmail.com>
2025-07-08 07:05:07 -0300

    Add files via upload

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática',
+    'Quanto é 111 + 111?',
+    'A resposta é 222'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é  HTML?',
+    'Linguagem padrão usada para criar e estruturar páginas web'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz tchau em Inglês?',
+    'bye'
+)
